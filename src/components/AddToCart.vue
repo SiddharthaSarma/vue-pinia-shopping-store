014181e16597785051ebd57f54af4d734d48f8d6
siddharthasarma <sidduinevitable@gmail.com>
2023-01-29 06:04:51 +0530

    add AddToCart component

---
+++ src/components/AddToCart.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="add-to-cart-btn">Add to cart</button>
+</template>
+<style scoped>
+.add-to-cart-btn {
+  padding: 0.5rem;
+  margin: 1rem;
+  cursor: pointer;
+}
+</style>
