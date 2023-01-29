f67ed4691c7b4af5277026c72c0183a04f35022f
siddharthasarma <sidduinevitable@gmail.com>
2023-01-29 06:00:48 +0530

    create base component for multiple products

---
+++ src/components/ProductsContainer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import products from "../data/products.json";
+import ProductCard from "./ProductCard.vue";
+</script>
+<template>
+  <div class="products-container">
+    <ProductCard
+      v-for="product in products"
+      :product="product"
+      :key="product.id"
+    />
+  </div>
+</template>
+<style scoped>
+.products-container {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
