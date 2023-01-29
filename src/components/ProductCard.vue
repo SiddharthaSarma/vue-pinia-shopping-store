04daf7873c7759fd6d2d5ef2037aa387b2be5c2a
siddharthasarma <sidduinevitable@gmail.com>
2023-01-29 06:00:16 +0530

    create base component for product display

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import ItemCounter from "./ItemCounter.vue";
+import AddToCart from "./AddToCart.vue";
+const getPath = (name: string) => {
+  return new URL(`../assets/${name}`, import.meta.url).href;
+};
+defineProps<{
+  product: {
+    id: number;
+    image: string;
+    name: string;
+    price: number;
+  };
+}>();
+</script>
+<template>
+  <div class="product">
+    <img :src="getPath(product.image)" width="225" height="200" />
+    <h3>{{ product.name }}</h3>
+    <span class="price">${{ product.price }}</span>
+    <ItemCounter />
+    <AddToCart />
+  </div>
+</template>
+<style scoped>
+.product {
+  border: 2px solid #ccc;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem;
+  position: relative;
+  box-shadow: rgba(151, 52, 52, 0.19) 0px 10px 20px,
+    rgba(0, 0, 0, 0.23) 0px 6px 6px;
+}
+.price {
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 5px;
+  background: green;
+  color: white;
+}
+</style>
