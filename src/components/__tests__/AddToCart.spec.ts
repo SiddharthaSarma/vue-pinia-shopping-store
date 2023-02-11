866c866c1be2485f5b727143789cdf1a0743b3a8
Siddhartha <sidduinevitable@gmail.com>
2023-02-11 13:53:55 +0000

    add test cases for AddToCart component

---
+++ src/components/__tests__/AddToCart.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import AddToCart from '../AddToCart.vue';
+
+describe('First test', () => {
+  it('first test', () => {
+    const wrapper = mount(AddToCart);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
